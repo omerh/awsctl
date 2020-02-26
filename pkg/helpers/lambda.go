1fc696c4234772765877af598add2307515ca0e1
Omer Haim <omerha@protonmail.com>
2020-02-26 11:13:13 +0200

    iterating nextMarker

--- pkg/helpers/lambda.go
+++ pkg/helpers/lambda.go
@@ -1,18 +1,28 @@
 package helpers
 
 import (
-	"fmt"
-
 	"github.com/aws/aws-sdk-go/service/lambda"
 )
 
 // GetAllLmbdasInRegion List all lambdas in a region
-func GetAllLmbdasInRegion(region string) {
+func GetAllLmbdasInRegion(region string) []*lambda.FunctionConfiguration {
 	awsSession, _ := InitAwsSession(region)
 	svc := lambda.New(awsSession)
 	input := &lambda.ListFunctionsInput{}
 	result, _ := svc.ListFunctions(input)
 
-	fmt.Println(result.Functions)
+	lambdas := result.Functions
 
+	// Iterate over NextMarker
+	for result.NextMarker != nil {
+		input = &lambda.ListFunctionsInput{
+			Marker: result.NextMarker,
+		}
+		result, _ = svc.ListFunctions(input)
+		for _, l := range result.Functions {
+			lambdas = append(lambdas, l)
+		}
+	}
+
+	return lambdas
 }
