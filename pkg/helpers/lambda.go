52b6de7ff85747884ebf4a6732162975ef2b5992
Omer Haim <omerha@protonmail.com>
2020-02-27 11:04:22 +0200

    fixed all

--- pkg/helpers/lambda.go
+++ pkg/helpers/lambda.go
@@ -1,11 +1,13 @@
 package helpers
 
 import (
+	"fmt"
+
 	"github.com/aws/aws-sdk-go/service/lambda"
 )
 
 // GetAllLmbdasInRegion List all lambdas in a region
-func GetAllLmbdasInRegion(region string) []*lambda.FunctionConfiguration {
+func GetAllLmbdasInRegion(region string, arn string) []*lambda.FunctionConfiguration {
 	awsSession, _ := InitAwsSession(region)
 	svc := lambda.New(awsSession)
 	input := &lambda.ListFunctionsInput{}
@@ -21,6 +23,18 @@ func GetAllLmbdasInRegion(region string) []*lambda.FunctionConfiguration {
 		result, _ = svc.ListFunctions(input)
 		for _, l := range result.Functions {
 			lambdas = append(lambdas, l)
+		}
+	}
+
+	// Filter out lambda
+	if len(arn) > 0 {
+		for _, l := range lambdas {
+			if *l.FunctionArn == arn {
+				fmt.Println("match")
+				lambdas = nil
+				lambdas = append(lambdas, l)
+				return lambdas
+			}
 		}
 	}
 
